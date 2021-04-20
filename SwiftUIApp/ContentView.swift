40ec669561385e441a66e8764f686f4ac2793780
Karan Gandhi <kag354@rutgers.edu>
2021-04-20 16:48:04 -0400

    Hello

--- SwiftUIApp/ContentView.swift
+++ SwiftUIApp/ContentView.swift
@@ -21,7 +21,7 @@ struct ContentView: View {
                 }
                 
             
-        Text("Hello World")
+        Text("Hello")
             .font(.body)
             .fontWeight(.medium)
             .foregroundColor(Color.green)
